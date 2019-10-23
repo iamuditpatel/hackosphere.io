8ec6f6e01824544ba9bc591ac558fb6d0994704a
Udit Patel <admin@uditpatel.com>
2019-10-23 11:14:17 +0530

    Update run-backup.php

--- run-backup.php
+++ run-backup.php
@@ -1,3 +1,41 @@
 <?PHP
 `./backup.sh`;
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.alert {
+    padding: 20px;
+    background-color: #42f436;
+    color: white;
+}
+
+.closebtn {
+    margin-left: 15px;
+    color: white;
+    font-weight: bold;
+    float: right;
+    font-size: 22px;
+    line-height: 20px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+
+.closebtn:hover {
+    color: black;
+}
+</style>
+</head>
+<body>
+
+
+<div class="alert">
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+  <strong>Success!</strong> Your snapshot of your website is successful created.
+</div>
+
+</body>
+</html>
